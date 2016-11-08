63d874a945322b143ec60c64c6bb615c1b8f23b6
nel215 <otomo.yuhei@gmail.com>
2016-11-08 22:50:42 +0900

    Ignore pep8 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name='change_finder',
     version='0.0.1',
-    description='An implementation of the algorithm for detecting outliers and change points from time series data.',
+    description='An implementation of the algorithm for detecting outliers and change points from time series data.',  # nopep8
     author='nel215',
     author_email='otomo.yuhei@gmail.com',
     url='https://github.com/nel215/change_finder',
