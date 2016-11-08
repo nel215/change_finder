754a1d19cad7ac482258a0a190942c8fb0ad6cad
nel215 <otomo.yuhei@gmail.com>
2016-11-08 22:44:37 +0900

    Add build pipeline

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
+clean:
+	rm -rf build/ dist/
+build: clean
+	python setup.py build bdist_wheel
 test:
 	PYTHONPATH=. py.test --pep8 .
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup
+
+
+setup(
+    name='change_finder',
+    version='0.0.1',
+    description='An implementation of the algorithm for detecting outliers and change points from time series data.',
+    author='nel215',
+    author_email='otomo.yuhei@gmail.com',
+    url='https://github.com/nel215/change_finder',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    packages=['change_finder'],
+    install_requires=[
+        "numpy",
+        "scipy",
+    ],
+    keywords=['machine learning'],
+)
